5635aba5221eca72bbedee158014308145004f0c
Gokulraj3249 <smartgokul7505@gmail.com>
2024-02-22 15:34:24 +0530

    first

---
+++ Branch.sql
@@ -0,0 +1,5 @@
+insert into branch values(1,'Insurance',20,'06.02.22'),
+						 (2,'Account',22,'01.04.22'),
+						 (3,'Energy',26,'24.02.19')
+						 
+						 
---
+++ Branch_Supplier.sql
@@ -0,0 +1,6 @@
+insert into branch_supplier values(1,'S_Direkt','Insurance'),
+								  (1,'Inshared','Insurance'),
+								  (2,'Deutsche_Bank','Credit'),
+								  (2,'Commerzbank','Credit'),
+								  (3,'Ostrom','Renewable'),
+								  (3,'E.On_Energy','Electricity')
