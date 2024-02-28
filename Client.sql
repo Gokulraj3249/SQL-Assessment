58bc184b6d016cd88ba85eca44c08232c7a17b0d
Gokulraj <149764910+Gokulraj3249@users.noreply.github.com>
2024-02-28 21:39:53 +0530

    Delete Client.sql

--- Client.sql
+++
@@ -1,8 +0,0 @@
-insert into client values(10,'Daniel','Maier',2),
-						 (11,'Ella','Walter',2),
-						 (12,'Zhang','Wei',1),
-						 (13,'Ivan','Kirillov',3),
-						 (14,'John','Smith',3),
-						 (15,'Dirk','Pelletr',1),
-						 (16,'Tobias','Roth',1),
-						 (27,'Leonie','Williams',1)
